6fc868dca1d5383f97559e18e24e19a6841c1dbe
Maria  Morris <mariasemailbox@gmail.com>
2018-04-02 23:56:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,14 @@ function logWhisper(string) {
   console.log("hello")
 }
 
+function sayHiToGrandma(string) {
+  if string.toLowerCase
+   return console.log("I can't hear you!") 
+}
+else if 
+string.toUpperCase
 
+   return console.log("YES INDEED!")
+    else 
+string.toMixedCase
+return console.log("I love you, too.")
