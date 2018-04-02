ffa79483927726b4891ed855839ed5ce31d7e475
Maria  Morris <mariasemailbox@gmail.com>
2018-04-02 23:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,8 @@ function logShout(string){
   return string.toUpperCase()
 }
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
+function logShout(string) { 
+  console.log("HELLO") 
+} 
 
 
