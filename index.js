d2c318db2dedd7f4737759cf41d47d144aadd458
Maria  Morris <mariasemailbox@gmail.com>
2018-04-03 00:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase)
-   return console.log("I can't hear you!") 
-} if (string.toUpperCase){
-   return console.log("YES INDEED!")
-  } else (string.toMixedCase){
-  return console.log("I love you, too.")
-}
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+ } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
